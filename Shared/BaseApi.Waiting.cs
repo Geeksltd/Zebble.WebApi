9946378e92a6cdbfa1a9cb4db0eaef24f85571ac
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2023-11-16 04:48:20 +0330

    Update deps

--- Shared/BaseApi.Waiting.cs
+++ Shared/BaseApi.Waiting.cs
@@ -1,37 +1,33 @@
 ﻿namespace Zebble
 {
-    using Newtonsoft.Json;
-    using System;
-    using System.Linq;
-    using System.Net;
-    using System.Net.Http;
-    using System.Net.Http.Headers;
     using System.Threading.Tasks;
 
     partial class BaseApi
     {
 #if MVVM
-      static Task HideWaiting()
-      {
-          Zebble.Mvvm.DialogViewModel.Current.HideWaiting();
-          return Task.CompletedTask;
-      }
+        static Task HideWaiting()
+        {
+            Zebble.Mvvm.DialogsViewModel.Current.HideWaiting();
+            return Task.CompletedTask;
+        }
 
-      static Task ShowWaiting()
-      {
-           Zebble.Mvvm.DialogViewModel.Current.ShowWaiting();
-           return Task.CompletedTask;
-      }
+        static Task ShowWaiting()
+        {
+            Zebble.Mvvm.DialogsViewModel.Current.ShowWaiting();
+            return Task.CompletedTask;
+        }
 
-      static Task Toast(string message)
-      {
-           Zebble.Mvvm.DialogViewModel.Current.Toast(message);
-           return Task.CompletedTask;
-      }
+        static Task Toast(string message)
+        {
+            Zebble.Mvvm.DialogsViewModel.Current.Toast(message);
+            return Task.CompletedTask;
+        }
 #else
-        static Task HideWaiting() => Waiting.Hide();
-        static Task ShowWaiting() => Waiting.Show();
-        static Task Toast(string message) => Alert.Toast(message);
+        static Task HideWaiting() => Dialogs.Current.HideWaiting();
+
+        static Task ShowWaiting() => Dialogs.Current.ShowWaiting();
+
+        static Task Toast(string message) => Dialogs.Current.Toast(message);
 #endif 
     }
 }
